36f22ce4fca727228f861e1b145e9bf45cedca5a
R4BBT <ryanfangk@gmail.com>
2021-01-21 14:15:10 +0000

    01/21/2021

--- monitoring-quickstart.sh
+++ monitoring-quickstart.sh
@@ -2,12 +2,15 @@
 curl -sSO "https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh"
 sudo bash add-monitoring-agent-repo.sh
 sudo apt-get update
-sudo apt-get install stackdriver-agent
+sudo apt-get install stackdriver-agent -y 
 sudo service stackdriver-agent start
 
 curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
 sudo bash add-logging-agent-repo.sh
 sudo apt-get update
-sudo apt-get install google-fluentd
-sudo apt-get install google-fluentd-catch-all-config-structured
+sudo apt-get install google-fluentd -y 
+sudo apt-get install google-fluentd-catch-all-config-structured -y 
 sudo service google-fluentd start
+
+sudo systemctl status stackdriver-agent
+sudo systemctl status google-fluentd
