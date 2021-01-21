cf5463791593eb699435c7117f4d1f85a0135914
R4BBT <ryanfangk@gmail.com>
2021-01-21 13:59:41 +0000

    01/21/2021

--- monitoring-quickstart.sh
+++ monitoring-quickstart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo curl -sSO https://dl.google.com/cloudagents/{add-logging-agent-repo, add-monitoring-agent-repo}.sh
+sudo curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+sudo curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
 sudo bash add-logging-agent-repo.sh add-monitoring-agent-repo.sh
 sudo apt-get update
 sudo apt-get install stackdriver-agent google-fluentd google-fluentd-catch-all-config-structured apache2 php7.0 -y
